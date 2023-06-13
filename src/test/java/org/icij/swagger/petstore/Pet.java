0eadea7aa2d37b76cf4195db82848fcfe8842d70
Bruno Thomas <bthomas@icij.org>
2023-06-13 13:39:55 +0000

    feat: add petstore example for tests
    Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com>

---
+++ src/test/java/org/icij/swagger/petstore/Pet.java
@@ -0,0 +1,77 @@
+package org.icij.swagger.petstore;
+
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "Pet")
+public class Pet {
+    private long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls = new ArrayList<String>();
+    private List<Tag> tags = new ArrayList<Tag>();
+    private String status;
+
+    @XmlElement(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @XmlElement(name = "category")
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @XmlElement(name = "name")
+    @Schema(example = "doggie", required = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlElementWrapper(name = "photoUrls")
+    @XmlElement(name = "photoUrl", required = true)
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    @XmlElementWrapper(name = "tags")
+    @XmlElement(name = "tag")
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    @XmlElement(name = "status")
+    @Schema(name = "pet status in the store", allowableValues = "available,pending,sold")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/org/icij/swagger/petstore/PetStoreResource.java
@@ -0,0 +1,69 @@
+package org.icij.swagger.petstore;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import net.codestory.http.WebServer;
+import net.codestory.http.annotations.*;
+
+import java.util.Map;
+
+import static net.codestory.http.errors.NotFoundException.notFoundIfNull;
+
+@Prefix("/api")
+public class PetStoreResource {
+    static StoreData storeData = new StoreData();
+    static PetData petData = new PetData();
+
+
+    @Operation(description = "Returns petstore home page")
+    @Get()
+    public String root() {
+        return "Fluent HTTP petstore";
+    }
+
+    @Operation(description = "Returns pet inventories by status",
+            summary = "Returns a map of status codes to quantities",
+            responses = { @ApiResponse(responseCode = "200") }
+    )
+    @Get("/inventory")
+    public Map<String, Integer> getInventory() {
+        return petData.getInventoryByStatus();
+    }
+
+    @Operation(summary = "Find purchase order by ID",
+            description = "For valid response try integer IDs with value >= 1 and <= 10. Other values will generated exceptions",
+            responses = { @ApiResponse(responseCode = "400", ref = "Invalid ID supplied"),
+                    @ApiResponse(responseCode = "404", ref = "Order not found") }
+    )
+    @Get("/order/:orderId")
+    public Order getOrderById(
+            @Parameter(name = "orderId", description = "ID of pet that needs to be fetched", required = true ) Long orderId) {
+        return notFoundIfNull(storeData.findOrderById(orderId));
+    }
+
+    @Post("/order")
+    @Operation(description = "Place an order for a pet")
+    @ApiResponses({ @ApiResponse(responseCode = "400", ref = "Invalid Order") })
+    public Order placeOrder(
+            @Parameter(description = "order placed for purchasing the pet", required = true) Order order) {
+        return storeData.placeOrder(order);
+    }
+
+    @Operation(description = "Delete purchase order by ID",
+            summary = "For valid response try integer IDs with positive integer value. Negative or non-integer values will generate API errors",
+            responses = { @ApiResponse(responseCode = "400", ref = "Invalid ID supplied"),
+                    @ApiResponse(responseCode = "404", ref = "Order not found") })
+    @Delete("/order/:orderId")
+    public boolean deleteOrder(
+            @Parameter(name="orderId", description = "ID of the order that needs to be deleted", required = true) Long orderId) {
+        return storeData.deleteOrder(orderId);
+    }
+
+    public static void main(String[] args) {
+        new WebServer().withSelectThreads(2).withThreadCount(10).configure(
+                routes -> routes.add(PetStoreResource.class)
+        ).start(12345);
+    }
+}
