08dc7ea9971595161f943f87fe4266e8314729b9
Bruno Thomas <bthomas@icij.org>
2023-06-27 17:05:54 +0000

    test: try to fix order for acceptance test
    Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com>

--- src/test/java/org/icij/swagger/AcceptanceTest.java
+++ src/test/java/org/icij/swagger/AcceptanceTest.java
@@ -2,6 +2,8 @@ package org.icij.swagger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import io.swagger.v3.core.util.ObjectMapperFactory;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.junit.Test;
@@ -14,12 +16,26 @@ import static org.junit.Assert.assertNotNull;
 public class AcceptanceTest {
     @Test
     public void test_populate_openapi() throws Exception {
-        ObjectMapper mapper = ObjectMapperFactory.createYaml31();
+        ObjectMapper mapper = createMapper();
         JsonNode expectedValue = mapper.readTree(getClass().getClassLoader().getResource("petstore/FullPetResource.yaml"));
         final FluentReader reader = new FluentReader();
         final OpenAPI openAPI = reader.read(findAllClassesUsingClassLoader("org.icij.swagger.petstore"));
 
         assertNotNull(openAPI);
-        assertEquals(mapper.valueToTree(openAPI).toString(), expectedValue.toString());
+        assertEquals(mapper.valueToTree(expectedValue).toString(), mapper.valueToTree(openAPI).toString());
+    }
+
+    private static ObjectMapper createMapper() {
+        YAMLFactory factory = new YAMLFactory() {
+            @Override
+            public boolean requiresPropertyOrdering() {
+                return true;
+            }
+        };
+        factory.disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
+        factory.enable(YAMLGenerator.Feature.MINIMIZE_QUOTES);
+        factory.enable(YAMLGenerator.Feature.SPLIT_LINES);
+        factory.enable(YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS);
+        return ObjectMapperFactory.create(factory, true);
     }
 }
