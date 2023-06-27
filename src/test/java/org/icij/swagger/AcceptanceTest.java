08d761442680bc1f07aec8f82b65361f385ef57d
Bruno Thomas <bthomas@icij.org>
2023-06-27 16:15:02 +0000

    test: try to fix ordering issue

--- src/test/java/org/icij/swagger/AcceptanceTest.java
+++ src/test/java/org/icij/swagger/AcceptanceTest.java
@@ -1,21 +1,25 @@
 package org.icij.swagger;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.util.ObjectMapperFactory;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.junit.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static org.icij.swagger.ClassUtils.findAllClassesUsingClassLoader;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
 public class AcceptanceTest {
     @Test
     public void test_populate_openapi() throws Exception {
+        ObjectMapper mapper = ObjectMapperFactory.createYaml31();
+        JsonNode expectedValue = mapper.readTree(getClass().getClassLoader().getResource("petstore/FullPetResource.yaml"));
         final FluentReader reader = new FluentReader();
         final OpenAPI openAPI = reader.read(findAllClassesUsingClassLoader("org.icij.swagger.petstore"));
+
         assertNotNull(openAPI);
-        SerializationMatchers.assertEqualsToYaml(openAPI, Files.readString(Paths.get(getClass().getClassLoader().getResource("petstore/FullPetResource.yaml").toURI())));
+        assertEquals(mapper.valueToTree(openAPI).toString(), expectedValue.toString());
     }
 }
